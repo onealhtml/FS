3d4691df64d7b59fd6d3649ec6d732fe53b686be
Lorenzo Farias <lorenzofarias8@hotmail.com>
2026-03-02 21:43:16 -0300

    fix logic in problem 1113 to correctly handle loop exit conditions

--- 1113.py
+++ 1113.py
@@ -8,7 +8,7 @@ def main():
     saida = 1          # Variável para controlar a saída do loop
     while saida == 1:   # Loop principal para ler os casos de teste
         x, y = entrada()
-        logica(x, y)
+        saida = logica(x, y)
 
 def entrada():
     x, y = map(int, input().split())
@@ -17,9 +17,13 @@ def entrada():
 def logica(x, y):
     if x < y:                           # Verifica se é crescente
         print("Crescente")
+        return 1
     elif x > y:                         # Verifica se é decrescente
         print("Decrescente")
-    else:                               # Caso x e y sejam iguais, sai do loop
-        saida = 0
+        return 1
+    elif x == y:                               # Caso x e y sejam iguais, sai do loop
+        return 0
+    return None
+
 
 main()  # Ponto de entrada do programa
